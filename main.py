314c25147b389b6f4d0d4d4e9769dcdda4efb87b
SirHarisTheGuy <haris.aliaa12@gmail.com>
2021-01-10 19:23:45 +0000

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import heartbeat
 
 
 
-cogs = ['cogs.pnw', 'cogs.debug', 'cogs.chat_monitor']
+cogs = ['cogs.pnw', 'cogs.debug', 'cogs.chat_monitor', 'cogs.fun']
 
 def get_prefix(bot, msg):
   prefixs = ["Rachel, ", "rachel, ","Rei, ", "rei, "]
